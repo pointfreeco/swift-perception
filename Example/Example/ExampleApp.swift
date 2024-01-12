5600d6aa018524e2555ac8a30a15ca372897aa3f
Stephen Celis <stephen@stephencelis.com>
2024-01-12 11:48:05 -0800

    Update ExampleApp.swift

--- Example/Example/ExampleApp.swift
+++ Example/Example/ExampleApp.swift
@@ -1,13 +1,10 @@
 import SwiftUI
-import Perception
 
 @main
 struct ExampleApp: App {
   var body: some Scene {
-    WithPerceptionTracking {
-      WindowGroup {
-        ContentView(model: CounterModel())
-      }
+    WindowGroup {
+      ContentView(model: CounterModel())
     }
   }
 }

ee20e238ec4b6ab386a2eea84a02dd5b888cb140
Stephen Celis <stephen@stephencelis.com>
2024-01-12 11:47:43 -0800

    Update ExampleApp.swift

--- Example/Example/ExampleApp.swift
+++ Example/Example/ExampleApp.swift
@@ -3,14 +3,10 @@ import Perception
 
 @main
 struct ExampleApp: App {
-  let model = CounterModel()
-
   var body: some Scene {
     WithPerceptionTracking {
-      let _ = print("!!!")
-//      let _ = model.count
       WindowGroup {
-        ContentView(model: model)
+        ContentView(model: CounterModel())
       }
     }
   }

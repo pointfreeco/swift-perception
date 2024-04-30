7ae342ffa771f6423e3b5555895be822c58f29ee
Stephen Celis <stephen@stephencelis.com>
2024-04-30 10:11:28 -0700

    Revert "Raise the minimum supported version of `Observable` to iOS 17.0.1 (#64)" (#67)
    This reverts commit 03d51ee56a5fa27daccbc9539cebe336e0b45bc0.

--- Sources/Perception/Macros.swift
+++ Sources/Perception/Macros.swift
@@ -12,10 +12,10 @@
 #if canImport(Observation)
   import Observation
 
-  @available(iOS, deprecated: 17.0.1, renamed: "Observable")
+  @available(iOS, deprecated: 17, renamed: "Observable")
   @available(macOS, deprecated: 14, renamed: "Observable")
-  @available(tvOS, deprecated: 17.0.1, renamed: "Observable")
-  @available(watchOS, deprecated: 10.0.1, renamed: "Observable")
+  @available(tvOS, deprecated: 17, renamed: "Observable")
+  @available(watchOS, deprecated: 10, renamed: "Observable")
   @attached(
     member, names: named(_$id), named(_$perceptionRegistrar), named(access), named(withMutation))
   @attached(memberAttribute)
@@ -23,19 +23,19 @@
   public macro Perceptible() =
     #externalMacro(module: "PerceptionMacros", type: "PerceptibleMacro")
 
-  @available(iOS, deprecated: 17.0.1, renamed: "ObservationTracked")
+  @available(iOS, deprecated: 17, renamed: "ObservationTracked")
   @available(macOS, deprecated: 14, renamed: "ObservationTracked")
-  @available(tvOS, deprecated: 17.0.1, renamed: "ObservationTracked")
-  @available(watchOS, deprecated: 10.0.1, renamed: "ObservationTracked")
+  @available(tvOS, deprecated: 17, renamed: "ObservationTracked")
+  @available(watchOS, deprecated: 10, renamed: "ObservationTracked")
   @attached(accessor, names: named(init), named(get), named(set))
   @attached(peer, names: prefixed(_))
   public macro PerceptionTracked() =
     #externalMacro(module: "PerceptionMacros", type: "PerceptionTrackedMacro")
 
-  @available(iOS, deprecated: 17.0.1, renamed: "ObservationIgnored")
+  @available(iOS, deprecated: 17, renamed: "ObservationIgnored")
   @available(macOS, deprecated: 14, renamed: "ObservationIgnored")
-  @available(tvOS, deprecated: 17.0.1, renamed: "ObservationIgnored")
-  @available(watchOS, deprecated: 10.0.1, renamed: "ObservationIgnored")
+  @available(tvOS, deprecated: 17, renamed: "ObservationIgnored")
+  @available(watchOS, deprecated: 10, renamed: "ObservationIgnored")
   @attached(accessor, names: named(willSet))
   public macro PerceptionIgnored() =
     #externalMacro(module: "PerceptionMacros", type: "PerceptionIgnoredMacro")

3aa58c9a775dc620c3f617f31c154f1dcf90446a
Stephen Celis <stephen@stephencelis.com>
2024-05-28 11:22:52 -0700

    wip

--- Sources/Perception/WithPerceptionTracking.swift
+++ Sources/Perception/WithPerceptionTracking.swift
@@ -157,14 +157,4 @@
       self.content = content
     }
   }
-
-  @available(iOS, deprecated: 17)
-  @available(macOS, deprecated: 14)
-  @available(tvOS, deprecated: 17)
-  @available(visionOS, deprecated: 9999)
-  @available(watchOS, deprecated: 10)
-  public enum _PerceptionLocals {
-    @TaskLocal public static var isInPerceptionTracking = false
-    @TaskLocal public static var skipPerceptionChecking = false
-  }
 #endif

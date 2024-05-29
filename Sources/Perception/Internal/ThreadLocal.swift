75af980f2803dbf65e68b5ea10727cefd9ddb4e2
Stephen Celis <stephen@stephencelis.com>
2024-05-29 10:25:17 -0700

    wip

--- Sources/Perception/Internal/ThreadLocal.swift
+++ Sources/Perception/Internal/ThreadLocal.swift
@@ -13,7 +13,7 @@ import Foundation
 
 struct _ThreadLocal {
   #if os(WASI)
-    static var value: UnsafeMutableRawPointer?
+    nonisolated(unsafe) static var value: UnsafeMutableRawPointer?
   #else
     static var value: UnsafeMutableRawPointer? {
       get { Thread.current.threadDictionary[Key()] as! UnsafeMutableRawPointer? }

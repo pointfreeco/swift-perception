662d90d90fbd098bc439a0a7f4f85d79eab1b147
Stephen Celis <stephen@stephencelis.com>
2024-05-30 12:50:47 -0700

    Make `Bindable` unavailable to visionOS
    Fixes #76.

--- Sources/Perception/Bindable.swift
+++ Sources/Perception/Bindable.swift
@@ -5,11 +5,26 @@
   /// perceptible objects.
   ///
   /// A backport of SwiftUI's `Bindable` property wrapper.
-  @available(iOS, introduced: 13, obsoleted: 17, message: "Use @Bindable without the 'Perception.' prefix.")
-  @available(macOS, introduced: 10.15, obsoleted: 14, message: "Use @Bindable without the 'Perception.' prefix.")
-  @available(tvOS, introduced: 13, obsoleted: 17, message: "Use @Bindable without the 'Perception.' prefix.")
-  @available(watchOS, introduced: 6, obsoleted: 10, message: "Use @Bindable without the 'Perception.' prefix.")
-  @available(visionOS, obsoleted: 9999, message: "Use @Bindable without the 'Perception.' prefix.")
+  @available(
+    iOS, introduced: 13, obsoleted: 17,
+    message: "Use '@Bindable' without the 'Perception.' prefix."
+  )
+  @available(
+    macOS, introduced: 10.15, obsoleted: 14,
+    message: "Use '@Bindable' without the 'Perception.' prefix."
+  )
+  @available(
+    tvOS, introduced: 13, obsoleted: 17,
+    message: "Use '@Bindable' without the 'Perception.' prefix."
+  )
+  @available(
+    visionOS, unavailable,
+    message: "Use '@Bindable' without the 'Perception.' prefix."
+  )
+  @available(
+    watchOS, introduced: 6, obsoleted: 10,
+    message: "Use '@Bindable' without the 'Perception.' prefix."
+  )
   @dynamicMemberLookup
   @propertyWrapper
   public struct Bindable<Value> {

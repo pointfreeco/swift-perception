d953be6a43045dbb8cc32148de45a464934df986
Stephen Celis <stephen@stephencelis.com>
2024-07-12 12:58:02 -0700

    Point updated packages to swiftlang organization

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     .library(name: "Perception", targets: ["Perception"])
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
     .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.1.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
   ],
   targets: [
     .target(

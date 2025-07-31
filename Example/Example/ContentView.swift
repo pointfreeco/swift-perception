0642503122fe1aaaad41473112c28d2e3a6bd829
Brandon Williams <mbrandonw@hey.com>
2025-07-31 12:25:22 -0500

    wip

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -3,13 +3,10 @@ import SwiftUI
 
 @Perceptible
 class CounterModel {
-  var count: Int = 0
+  var count = 0
   var isDisplayingCount = true
   var isPresentingSheet = false
   var text = ""
-
-
-
   func decrementButtonTapped() {
     withAnimation {
       count -= 1
@@ -29,15 +26,47 @@ struct ContentView: View {
   @Perception.Bindable var model: CounterModel
 
   var body: some View {
-    Form {
-      WithPerceptionTracking {
-        Text(model.count.description)
-          .font(.largeTitle)
+    WithPerceptionTracking {
+      let _ = print("\(Self.self): tracked change.")
+      Form {
+        TextField("Text", text: $model.text)
+        if model.isDisplayingCount {
+          Text(model.count.description)
+            .font(.largeTitle)
+        } else {
+          Text("Not tracking count")
+            .font(.largeTitle)
+        }
         Button("Decrement") { model.decrementButtonTapped() }
         Button("Increment") { model.incrementButtonTapped() }
+        Toggle(isOn: $model.isDisplayingCount.animation()) {
+          Text("Display count?")
+        }
+        Button("Present sheet") {
+          model.presentSheetButtonTapped()
+        }
       }
-      Text(model.count.description)
-        .font(.largeTitle)
+      .sheet(isPresented: $model.isPresentingSheet) {
+        if #available(iOS 16.0, *) {
+          WithPerceptionTracking {
+            Form {
+              Text(model.count.description)
+                .font(.largeTitle)
+              Button("Decrement") { model.decrementButtonTapped() }
+              Button("Increment") { model.incrementButtonTapped() }
+            }
+            .presentationDetents([.medium])
+          }
+        } else {
+          WithPerceptionTracking {
+            Form {
+              Text(model.count.description)
+              Button("Decrement") { model.decrementButtonTapped() }
+              Button("Increment") { model.incrementButtonTapped() }
+            }
+          }
+        }
+      }
     }
   }
 }

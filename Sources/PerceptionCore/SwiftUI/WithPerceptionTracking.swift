602e9c10a5bdd58f73c72b92728034ff38d69466
Stephen Celis <stephen@stephencelis.com>
2025-08-27 11:52:49 -0700

    Remove WithPerceptionTracking's MapKit support (#158)
    This was added in #134 but given the availability matches the
    availability of Observation, it shouldn't be necessary.
    
    It also seems to introduce problems with some build systems, like Skip.

--- Sources/PerceptionCore/SwiftUI/WithPerceptionTracking.swift
+++ Sources/PerceptionCore/SwiftUI/WithPerceptionTracking.swift
@@ -219,15 +219,4 @@
       }
     }
   #endif
-
-  #if canImport(MapKit)
-    import MapKit
-
-    @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
-    extension WithPerceptionTracking: MapContent where Content: MapContent {
-      public init(@MapContentBuilder content: @escaping () -> Content) {
-        self.init(content: content())
-      }
-    }
-  #endif
 #endif

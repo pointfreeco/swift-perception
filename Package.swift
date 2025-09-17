30721accd0370d7c9cb5bd0f7cdf5a1a767b383d
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2025-09-17 13:45:28 -0500

    Support SwiftSyntax 602. (#163)

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.6.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.6.0"),
-    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"602.0.0"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"603.0.0"),
   ],
   targets: [
     .target(
